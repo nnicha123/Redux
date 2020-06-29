155ede8c57ccfb05f289faef411cb367efad78b7
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-29 12:05:15 +0700

    using one reducer

--- redux-overview/index.js
+++ redux-overview/index.js
@@ -2,6 +2,7 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 buyCake = () => {
     return {
@@ -9,9 +10,16 @@ buyCake = () => {
         info: 'First redux action'
     }
 }
+buyIceCream = () => {
+    return {
+        type: BUY_ICECREAM,
+        info: 'Second redux action'
+    }
+}
 
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCreams:20
 }
 
 const reducer = (state = initialState, action) => {
@@ -19,6 +27,10 @@ const reducer = (state = initialState, action) => {
         case BUY_CAKE: return{
             ...state,
             numOfCakes:state.numOfCakes -1
+        }
+        case BUY_ICECREAM: return{
+            ...state,
+            numOfIceCreams:state.numOfIceCreams -1
         }
         default: return state
     }
@@ -29,7 +41,9 @@ console.log('Initial state', store.getState())
 const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
 
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
 
 unsubscribe()
