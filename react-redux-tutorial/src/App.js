272db8c654688087eb72af4895fb2d145ea9973d
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-13 16:27:42 +0700

    added tutorial 2

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -4,29 +4,54 @@ import './App.css';
 import { connect } from 'react-redux'
 
 class App extends Component {
+  state = {
+    name: '',
+    email: ''
+  }
+  addHistory = (e) => {
+    e.preventDefault();
+    
+    this.props.addHistory({ type: "ADD_HISTORY", name: this.state.name, email: this.state.email })
+    this.setState({name:'',email:''})
+  }
   render() {
-    return(
+    return (
       <div className="App">
         {this.props.data}
-        <button onClick={() => this.props.plus({type:"INCREASE"})}>+</button>
-        <button onClick={() => this.props.minus({type:"DECREASE"})}>-</button>
-        <button onClick={() => this.props.reset({type:"RESET"})}>Reset</button>
+        <button onClick={() => this.props.plus({ type: "INCREASE" })}>+</button>
+        <button onClick={() => this.props.minus({ type: "DECREASE" })}>-</button>
+        <button onClick={() => this.props.reset({ type: "RESET" })}>Reset</button>
+        <button onClick={() => this.props.addNum({ type: "ADD_NUM", num: this.props.data })}>addNum</button>
+        <button onClick={() => this.props.addPerson({ type: "ADD_PERSON" })}>addPerson</button>
+        <ul>
+          {this.props.state.arr.map((num, idx) => <li key={idx}>{num}</li>)}
+    {this.props.state.arrObj.map((obj, idx) => <li key={idx}>Name: {obj.name}, EMail: {obj.email}</li>)}
+        </ul>
+        <form onSubmit={this.addHistory}>
+          <input type="text" placeholder="name" onChange={(e) => this.setState({ name: e.target.value })} value={this.state.name} />
+          <input type="email" placeholder="email" onChange={(e) => this.setState({ email: e.target.value })} value={this.state.email} />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
 }
 const mapStateToProps = state => {
   return {
-    data:state.count
+    data: state.count,
+    state: state
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    plus:(action) => dispatch(action),
-    minus:(action) => dispatch(action),
-    reset:(action) => dispatch(action)
+    plus: (action) => dispatch(action),
+    minus: (action) => dispatch(action),
+    reset: (action) => dispatch(action),
+    addNum: (action) => dispatch(action),
+    addPerson: (action) => dispatch(action),
+    addHistory: (action) => dispatch(action)
   }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- react-redux-tutorial/src/reducer.js
+++ react-redux-tutorial/src/reducer.js
@@ -1,13 +1,24 @@
 const initialState = {
-    count:0
+    count: 0,
+    arr: [],
+    arrObj: []
 }
 
-const reducer = (state = initialState,action) => {
-    switch(action.type){
-        case 'INCREASE': 
-        return {...state,count:state.count+1};
-        case 'DECREASE': return {...state,count:state.count -1};
-        case 'RESET': return {...state,count:0};
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREASE':
+            return { ...state, count: state.count + 1 };
+        case 'DECREASE': return { ...state, count: state.count - 1 };
+        case 'RESET': return { ...state, count: 0 };
+        case "ADD_NUM":
+            const newArray = [...state.arr, action.num]
+            return { ...state, arr: newArray }
+        case "ADD_PERSON":
+            const newObj = [...state.arrObj, { name: 'John' }];
+            return { ...state, arrObj: newObj }
+        case "ADD_HISTORY":
+            const history = [...state.arrObj, { name: action.name, email: action.email }]
+            return { ...state, arrObj: history }
         default: return state
     }
 }
