60dee1dd5a888ab32d95837048e4609dde8880c5
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-13 16:39:49 +0700

    changed state to reducer for text name and text email

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -10,9 +10,8 @@ class App extends Component {
   }
   addHistory = (e) => {
     e.preventDefault();
-    
-    this.props.addHistory({ type: "ADD_HISTORY", name: this.state.name, email: this.state.email })
-    this.setState({name:'',email:''})
+
+    this.props.addHistory({ type: "ADD_HISTORY", name: this.props.state.textName, email: this.props.state.textEmail })
   }
   render() {
     return (
@@ -25,11 +24,11 @@ class App extends Component {
         <button onClick={() => this.props.addPerson({ type: "ADD_PERSON" })}>addPerson</button>
         <ul>
           {this.props.state.arr.map((num, idx) => <li key={idx}>{num}</li>)}
-    {this.props.state.arrObj.map((obj, idx) => <li key={idx}>Name: {obj.name}, EMail: {obj.email}</li>)}
+          {this.props.state.arrObj.map((obj, idx) => <li key={idx}>Name: {obj.name}, EMail: {obj.email}</li>)}
         </ul>
         <form onSubmit={this.addHistory}>
-          <input type="text" placeholder="name" onChange={(e) => this.setState({ name: e.target.value })} value={this.state.name} />
-          <input type="email" placeholder="email" onChange={(e) => this.setState({ email: e.target.value })} value={this.state.email} />
+          <input type="text" placeholder="name" onChange={(e) => this.props.textName({ type:"TEXT_NAME",textName:e.target.value })} value={this.props.state.textName} />
+          <input type="email" placeholder="email" onChange={(e) => this.props.textEmail({ type:"TEXT_EMAIL",textEmail:e.target.value })} value={this.props.state.textEmail} />
           <button type="submit">Submit</button>
         </form>
       </div>
@@ -50,7 +49,9 @@ const mapDispatchToProps = dispatch => {
     reset: (action) => dispatch(action),
     addNum: (action) => dispatch(action),
     addPerson: (action) => dispatch(action),
-    addHistory: (action) => dispatch(action)
+    addHistory: (action) => dispatch(action),
+    textName:(action) => dispatch(action),
+    textEmail:(action) => dispatch(action)
   }
 }
 
--- react-redux-tutorial/src/reducer.js
+++ react-redux-tutorial/src/reducer.js
@@ -1,7 +1,9 @@
 const initialState = {
     count: 0,
     arr: [],
-    arrObj: []
+    arrObj: [],
+    textName: '',
+    textEmail: ''
 }
 
 const reducer = (state = initialState, action) => {
@@ -16,9 +18,14 @@ const reducer = (state = initialState, action) => {
         case "ADD_PERSON":
             const newObj = [...state.arrObj, { name: 'John' }];
             return { ...state, arrObj: newObj }
+        case"TEXT_NAME":
+            return {...state, textName:action.textName}
+        case "TEXT_EMAIL":
+            return {...state, textEmail:action.textEmail}
+
         case "ADD_HISTORY":
             const history = [...state.arrObj, { name: action.name, email: action.email }]
-            return { ...state, arrObj: history }
+            return { ...state, arrObj: history,textName:'',textEmail:'' }
         default: return state
     }
 }
